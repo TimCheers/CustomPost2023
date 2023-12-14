97ec337353a1dddc158041b0286752b2f4675a2b
TimCheersNote <artem1978354@gmail.com>
2023-12-14 16:35:58 +0500

    тут хоть отчисляйся

--- CustomPost2023/obj/Debug/net6.0/CustomPost2023.AssemblyInfo.cs
+++ CustomPost2023/obj/Debug/net6.0/CustomPost2023.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CustomPost2023")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6be81f8bb139b47e74ad2bef7869a3908a9a4cf5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CustomPost2023")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CustomPost2023")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
