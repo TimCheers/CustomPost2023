5a9bd552a96c39cd400d9d9b3d1fbc72a1821304
osheat <oleg9600@gmail.com>
2023-12-22 18:25:56 +0500

    Добавил стили к формам админа

--- CustomPost2023/obj/Debug/net6.0/CustomPost2023.AssemblyInfo.cs
+++ CustomPost2023/obj/Debug/net6.0/CustomPost2023.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CustomPost2023")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd101cd1faf23cc732ec51255ebc33e540886e48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+608e171f398e12ef55eb90c786dfc0e4820980a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CustomPost2023")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CustomPost2023")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
